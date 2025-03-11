ea3e9cd271521485cfdb4f601ac40caf581f274a
danny-gomes <dannnygomes@outlook.pt>
2025-03-11 22:36:59 +0000

    Implementado metodo para alterar a classe da classe GrupoAutomovel

--- DEMO_ORM/src/main/java/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
+++ DEMO_ORM/src/main/java/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
@@ -3,8 +3,13 @@ package isep.eapli.demo_orm.dominio;
 public class GrupoAutomovel {
 
     private int numeroPortas;
+    private String classe;
 
     public void setNumeroPortas(int numeroPortas) {
         this.numeroPortas = numeroPortas;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
     }
 }
