5c1ebcdd30511025b1e9f830cfc190b31bb44800
danny-gomes <dannnygomes@outlook.pt>
2025-03-11 22:36:02 +0000

    Implementado metodo para alterar o numero de portas da classe GrupoAutomovel

--- DEMO_ORM/src/main/java/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
+++ DEMO_ORM/src/main/java/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
@@ -2,4 +2,9 @@ package isep.eapli.demo_orm.dominio;
 
 public class GrupoAutomovel {
 
+    private int numeroPortas;
+
+    public void setNumeroPortas(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
 }
